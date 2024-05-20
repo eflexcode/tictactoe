fe9a8997b6d7c6ff55a5bf43dbad58c218ddcda4
Eze Larry Ifeanyi <ezelarry379@gmail.com>
2024-05-20 15:34:26 +0100

    working on game play

--- src/main/java/com/ifeanyi/tictactoe/games/controller/GameController.java
+++ src/main/java/com/ifeanyi/tictactoe/games/controller/GameController.java
@@ -1,9 +1,9 @@
 package com.ifeanyi.tictactoe.games.controller;
 
+import com.ifeanyi.tictactoe.exception.NotFoundException;
 import com.ifeanyi.tictactoe.games.entity.Game;
+import com.ifeanyi.tictactoe.games.model.JoinGame;
 import com.ifeanyi.tictactoe.games.service.GameService;
-import com.ifeanyi.tictactoe.users.entity.User;
-import com.ifeanyi.tictactoe.users.model.CreateUserModel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,14 +18,26 @@ public class GameController {
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.OK)
     public Game create(@RequestBody Game game) {
-        System.out.println("ssssssssssssssssssssss");
         return gameService.create(game);
     }
 
     @PostMapping("/join")
     @ResponseStatus(HttpStatus.OK)
-    public Game join(@RequestBody Game game) {
-        System.out.println("ssssssssssssssssssssss");
-        return gameService.create(game);
+    public Game join(@RequestBody JoinGame game) throws NotFoundException {
+        return gameService.join(game);
     }
+
+    @GetMapping("/get/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Game get(@PathVariable String id) throws NotFoundException {
+        return gameService.get(id);
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void delete(@PathVariable String id) throws NotFoundException {
+         gameService.delete(id);
+    }
+
 }
